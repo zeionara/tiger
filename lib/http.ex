c66d638d947dc1e20964d3c307725401af32d425
Zeio Nara <zeionara@gmail.com>
2023-06-28 00:06:08 +0300

    refactor(http): updated http api implementation

--- lib/http.ex
+++ lib/http.ex
@@ -3,8 +3,14 @@ defmodule Http do
   Basic http methods for working with rest apis
   """
 
-  def get(url) do
-    %{status_code: code, body: body} = HTTPoison.get!(url)
+  defp push_params(url, params) do
+    "#{url}?#{URI.encode_query(params)}"
+  end
+
+  def get(url, opts \\ []) do
+    params = Keyword.get(opts, :params, %{})
+
+    %{status_code: code, body: body} = url |> push_params(params) |> HTTPoison.get!
 
     case code do
       200 -> {:ok, Poison.decode!(body)}
@@ -16,7 +22,7 @@ defmodule Http do
     params = Keyword.get(opts, :params, %{})
     body = Keyword.get(opts, :body, %{})
 
-    %{status_code: code, body: body} = HTTPoison.post!("#{url}&#{URI.encode_query(params)}", body |> Poison.encode!)
+    %{status_code: code, body: body} = url |> push_params(params) |> HTTPoison.post!(body |> Poison.encode!)
 
     case code do
       200 -> {:ok, Poison.decode!(body)}
--- lib/trello.ex
+++ lib/trello.ex
@@ -3,18 +3,47 @@ defmodule Trello do
   @api_token System.get_env("TRELLO_TOKEN")
   @api_version "1"
 
-  @base "https://api.trello.com/#{@api_version}"
-  @auth "key=#{@api_key}&token=#{@api_token}"
+  @root "https://api.trello.com/#{@api_version}"
+  @auth %{ "key" => @api_key, "token" => @api_token } # "key=#{@api_key}&token=#{@api_token}"
+
+  defp get(url, opts \\ []) do
+    case Keyword.get(opts, :params) do
+      nil -> Http.get(url, params: @auth)
+      params -> Http.get(url, params: Map.merge(@auth, params))
+    end
+  end
+
+  # defp post(url, opts \\ []) do
+  defp post(url, opts) do
+    case Keyword.get(opts, :params) do
+      nil ->
+        case Keyword.get(opts, :body) do
+          nil -> Http.post(url, params: @auth)
+          body -> Http.post(url, params: @auth, body: body)
+        end
+      params ->
+        params = Map.merge(@auth, params)
+
+        case Keyword.get(opts, :body) do
+          nil -> Http.post(url, params: params)
+          body -> Http.post(url, params: params, body: body)
+        end
+    end
+  end
 
   def get_board(id) do
-    Http.get("#{@base}/boards/#{id}?#{@auth}")
+    get("#{@root}/boards/#{id}")
   end
 
   def get_lists(board) do
-    Http.get("#{@base}/boards/#{board}/lists?#{@auth}")
+    get("#{@root}/boards/#{board}/lists")
   end
 
   def create_card(list: list, name: name) do
-    Http.post("#{@base}/cards/?idList=#{list}&#{@auth}", params: %{"name" => name, "desc" => "#{name} description"})
+    # post("#{@root}/cards", params: %{"idList" => list, "name" => name, "desc" => "#{name} description"})
+    case post("#{@root}/cards", params: %{"idList" => list, "name" => name, "desc" => "#{name} description"}) do
+      {:ok, card} -> {:ok, card["id"]}
+      {:error, _} -> {:error, "failed to create card"}
+    end
   end
 end
